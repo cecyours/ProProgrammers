8a58f5505d0d5d5695bf4dff054061d7886e4458
gkhell <gpkmyworld@gmail.com>
2023-02-18 22:07:46 -0800

    18/02/2023⠀⎦⎡⠀22:07⠀⎦⎢⠀Saturday

---
+++ S01_P01_arithmetic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b;
+	char ch;
+	
+	printf("Enter expression : ");
+	scanf("%d%c%d",&a,&ch,&b);
+	
+	
+	switch(ch)
+	{
+		case '+':printf("%d + %d = %d\n",a,b,a+b);break;
+		case '-':printf("%d - %d = %d\n",a,b,a-b);break;
+		case '*':printf("%d * %d = %d\n",a,b,a*b);break;
+		case '/':printf("%d / %d = %d\n",a,b,a/b);break;
+		case '%':printf("%d %% %d = %d\n",a,b,a%b);break;
+	}
+	
+	return 'A';
+}
